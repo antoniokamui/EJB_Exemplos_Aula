c941421095b133973ac3dda6afa5e391f661975d
RM30316 <antonio.kamui@gmail.com>
2016-08-17 20:49:58 -0300

    start

---
+++ 01_FolhaPagamentoClient/ejbModule/br/com/fiap/interfaces/FolhaPagamento.java
@@ -0,0 +1,10 @@
+package br.com.fiap.interfaces;
+
+public interface FolhaPagamento {
+	
+	void setSalario(double salario);
+	void setDesconto(double taxa);
+	double calcularINSS();
+	double calcularSalarioLiquido();
+	
+}
