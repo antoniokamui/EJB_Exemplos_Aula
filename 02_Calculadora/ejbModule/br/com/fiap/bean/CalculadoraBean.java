78e318a94f6fa969e5099ff538f3c2a1b488d8dc
RM30316 <antonio.kamui@gmail.com>
2016-08-17 21:20:38 -0300

    exercicio2

---
+++ 02_Calculadora/ejbModule/br/com/fiap/bean/CalculadoraBean.java
@@ -0,0 +1,27 @@
+package br.com.fiap.bean;
+
+import javax.ejb.Stateless;
+import br.com.fiap.interfaces.CalculadoraLocal;
+
+@Stateless
+public class CalculadoraBean implements CalculadoraLocal {
+	@Override
+	public double somar(double x, double y) {
+		return x + y;
+	}
+
+	@Override
+	public double subtrair(double x, double y) {
+		return x - y;
+	}
+
+	@Override
+	public double multiplicar(double x, double y) {
+		return x * y;
+	}
+
+	@Override
+	public double dividir(double x, double y) {
+		return x / y;
+	}
+}
